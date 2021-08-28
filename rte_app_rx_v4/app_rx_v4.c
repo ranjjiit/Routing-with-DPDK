52ec0ab106de47f3557b20d23d8e011d86f2b6d5
root <root@node1-2.sb9.orbit-lab.org>
2021-08-28 03:20:09 +0000

    Sender logic added to the forwarder

---
+++ rte_app_rx_v4/app_rx_v4.c
@@ -0,0 +1,259 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2010-2015 Intel Corporation
+ */
+
+#include <stdint.h>
+#include <inttypes.h>
+#include <rte_eal.h>
+#include <rte_ethdev.h>
+#include <rte_cycles.h>
+#include <rte_lcore.h>
+#include <rte_mbuf.h>
+#include <unistd.h>
+#include <rte_common.h>
+#include <rte_hash.h>
+#include <rte_jhash.h>
+#include <rte_mbuf_dyn.h>
+
+#define RX_RING_SIZE 1024
+#define TX_RING_SIZE 1024
+
+#define NUM_MBUFS 8191
+#define MBUF_CACHE_SIZE 250
+#define BURST_SIZE 256
+#define PTP_PROTOCOL 0x88F7
+uint64_t rx_count; // global variable to keep track of the number of received packets (to be displayed every second)
+uint64_t max_packets = 100000;
+
+static const struct rte_eth_conf port_conf_default = {
+	.rxmode = {
+		.max_rx_pkt_len = RTE_ETHER_MAX_LEN,
+	},
+};
+
+
+/* basicfwd.c: Basic DPDK skeleton forwarding example. */
+
+/*
+ * Initializes a given port using global settings and with the RX buffers
+ * coming from the mbuf_pool passed as a parameter.
+ */
+static inline int
+port_init(uint16_t port, struct rte_mempool *mbuf_pool)
+{
+	struct rte_eth_conf port_conf = port_conf_default;
+	const uint16_t rx_rings = 1, tx_rings = 1;
+	uint16_t nb_rxd = RX_RING_SIZE;
+	uint16_t nb_txd = TX_RING_SIZE;
+	int retval;
+	uint16_t q;
+	struct rte_eth_dev_info dev_info;
+	struct rte_eth_txconf txconf;
+
+	if (!rte_eth_dev_is_valid_port(port))
+		return -1;
+
+	retval = rte_eth_dev_info_get(port, &dev_info);
+	if (retval != 0) {
+		printf("Error during getting device (port %u) info: %s\n",
+				port, strerror(-retval));
+		return retval;
+	}
+
+	if (dev_info.tx_offload_capa & DEV_TX_OFFLOAD_MBUF_FAST_FREE)
+		port_conf.txmode.offloads |=
+			DEV_TX_OFFLOAD_MBUF_FAST_FREE;
+
+	/* Configure the Ethernet device. */
+	retval = rte_eth_dev_configure(port, rx_rings, tx_rings, &port_conf);
+	if (retval != 0)
+		return retval;
+
+	retval = rte_eth_dev_adjust_nb_rx_tx_desc(port, &nb_rxd, &nb_txd);
+	if (retval != 0)
+		return retval;
+
+	/* Allocate and set up 1 RX queue per Ethernet port. */
+	for (q = 0; q < rx_rings; q++) {
+		retval = rte_eth_rx_queue_setup(port, q, nb_rxd,
+				rte_eth_dev_socket_id(port), NULL, mbuf_pool);
+		if (retval < 0)
+			return retval;
+	}
+
+	txconf = dev_info.default_txconf;
+	txconf.offloads = port_conf.txmode.offloads;
+	/* Allocate and set up 1 TX queue per Ethernet port. */
+	for (q = 0; q < tx_rings; q++) {
+		retval = rte_eth_tx_queue_setup(port, q, nb_txd,
+				rte_eth_dev_socket_id(port), &txconf);
+		if (retval < 0)
+			return retval;
+	}
+
+	/* Start the Ethernet port. */
+	retval = rte_eth_dev_start(port);
+	if (retval < 0)
+		return retval;
+
+	/* Display the port MAC address. */
+	struct rte_ether_addr addr;
+	retval = rte_eth_macaddr_get(port, &addr);
+	if (retval != 0)
+		return retval;
+
+	printf("Receiver is Port %u with MAC address: %02" PRIx8 " %02" PRIx8 " %02" PRIx8
+			   " %02" PRIx8 " %02" PRIx8 " %02" PRIx8 "\n",
+			port,
+			addr.addr_bytes[0], addr.addr_bytes[1],
+			addr.addr_bytes[2], addr.addr_bytes[3],
+			addr.addr_bytes[4], addr.addr_bytes[5]);
+
+	/* Enable RX in promiscuous mode for the Ethernet device. */
+	retval = rte_eth_promiscuous_enable(port);
+	if (retval != 0)
+		return retval;
+        
+//        /* RX and TX callbacks are added to the ports. 8< */
+//	rte_eth_add_rx_callback(0, 0, add_timestamps, NULL);
+//	rte_eth_add_tx_callback(0, 0, calc_latency, NULL);
+//	/* >8 End of RX and TX callbacks. */
+
+	return 0;
+}
+
+static int
+lcore_stat(__rte_unused void *arg)
+{
+    for(; ;)
+    {
+        sleep(2); // report stats every second
+        printf("Number of packets received %"PRIu64 "\n", rx_count);
+    }
+}
+struct my_message{
+    struct rte_ether_hdr eth_hdr;
+    uint16_t type;
+    uint16_t dst_addr;
+    //uint16_t src_addr;
+    uint32_t seqNo;
+    uint64_t timestamp;
+    char payload[10];
+};
+
+
+void my_receive()
+{
+    int retval;
+    struct my_message *my_pkt;
+    uint16_t eth_type; 
+    rx_count = 0;
+    
+    printf("\nCore %u receiving packets. [Ctrl+C to quit]\n",
+                    rte_lcore_id());
+    
+    /* Run until the application is quit or killed. */
+    do{
+        
+        /* Get burst of RX packets, from first port of pair. */
+        struct rte_mbuf *bufs[BURST_SIZE];
+        uint16_t nb_rx = rte_eth_rx_burst(0, 0,
+                        bufs, BURST_SIZE);
+        
+        if (unlikely(nb_rx == 0))
+                continue;
+        
+        printf("\nNumber of packets received at machine 2 is %"PRIu16"\n", nb_rx);
+        
+        for(int i = 0; i < nb_rx; i++)
+        {
+            my_pkt = rte_pktmbuf_mtod(bufs[i], struct my_message *);
+            eth_type = rte_be_to_cpu_16(my_pkt->eth_hdr.ether_type);
+            
+            /* Check for data packet of interest and ignore other broadcasts 
+             messages */
+            
+            if(likely(eth_type == PTP_PROTOCOL))
+            {
+                rx_count = rx_count + 1;
+                rte_ether_addr_copy(&my_pkt->eth_hdr.s_addr, &my_pkt->eth_hdr.d_addr);
+                rte_ether_addr_copy(&my_pkt->eth_hdr.d_addr, &my_pkt->eth_hdr.s_addr);
+            }
+            
+        }
+        
+        
+        uint16_t nb_tx = rte_eth_tx_burst(0, 0, bufs, nb_rx);
+        printf("\nNumber of packets transmitted from machine 2 is %"PRIu16"\n", nb_tx);
+        
+        if(unlikely(nb_tx < nb_rx))
+        {
+            uint16_t buf;
+            for(buf = nb_tx; buf < nb_rx; buf++)
+                rte_pktmbuf_free(bufs[buf]);
+        }
+        
+    }while(rx_count < max_packets);
+}
+
+/*
+ * The main function, which does initialization and calls the per-lcore
+ * functions.
+ */
+int
+main(int argc, char *argv[])
+{
+    struct rte_mempool *mbuf_pool;
+    unsigned nb_ports;
+    uint16_t portid;
+    uint16_t port;
+    unsigned lcore_id;
+
+    /* Initialize the Environment Abstraction Layer (EAL). */
+    int ret = rte_eal_init(argc, argv);
+    if (ret < 0)
+            rte_exit(EXIT_FAILURE, "Error with EAL initialization\n");
+
+    argc -= ret;
+    argv += ret;
+
+    /* Get the number of ports */
+    nb_ports = rte_eth_dev_count_avail();
+
+    /* Creates a new mempool in memory to hold the mbufs. */
+    mbuf_pool = rte_pktmbuf_pool_create("MBUF_POOL", NUM_MBUFS * nb_ports,
+            MBUF_CACHE_SIZE, 0, RTE_MBUF_DEFAULT_BUF_SIZE, rte_socket_id());
+
+    if (mbuf_pool == NULL)
+            rte_exit(EXIT_FAILURE, "Cannot create mbuf pool\n");
+
+    /* Initialize all ports. */
+    RTE_ETH_FOREACH_DEV(portid)
+            if (port_init(portid, mbuf_pool) != 0)
+                    rte_exit(EXIT_FAILURE, "Cannot init port %"PRIu16 "\n",
+                                    portid);
+    
+    /*
+     * Check that the port is on the same NUMA node as the polling thread
+     * for best performance.
+     */
+    RTE_ETH_FOREACH_DEV(port)
+            if (rte_eth_dev_socket_id(port) > 0 &&
+                            rte_eth_dev_socket_id(port) !=
+                                            (int)rte_socket_id())
+                    printf("WARNING, port %u is on remote NUMA node to "
+                                    "polling thread.\n\tPerformance will "
+                                    "not be optimal.\n", port);
+
+
+//    lcore_id = rte_get_next_lcore(-1, 1, 0);
+//    if(lcore_id == RTE_MAX_LCORE)
+//    {
+//        rte_exit(EXIT_FAILURE, "Slave core id required!");
+//    }
+//    rte_eal_remote_launch(lcore_stat, NULL, lcore_id);
+    
+    my_receive();
+    
+    return 0;
+}
